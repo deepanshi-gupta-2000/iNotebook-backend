cc39d9ae26e18f54226a9b179cede7b0edd59f64
deepanshi-gupta-2000 <2000deepanshigupta@gmail.com>
2024-05-12 13:16:15 +0530

    minor adjustment done acc to UI. Adding success in response

--- routes/auth.js
+++ routes/auth.js
@@ -17,17 +17,18 @@ router.post('/signup',
         body('password', 'invalid password').isLength({ min: 5 })
     ],
     async (req, res) => {
+        let success = false;
         //Check if user did any mistake in entering fields.
         const result = validationResult(req);
         if (!result.isEmpty()) {
-            return res.status(400).json({ errors: result.array() });
+            return res.status(400).json({success, errors: result.array() });
         }
         //If there are errors return bad request and errors
         try {
             // Check user with this email id exists
             let user = await User.findOne({ email: req.body.email })
             if (user) {
-                return res.status(400).json({ 'error': 'sorry a user exists with this email' })
+                return res.status(400).json({success, 'error': 'sorry a user exists with this email' })
             }
 
             //Create secure Password
@@ -44,7 +45,8 @@ router.post('/signup',
                 id: user.id
             }
             const auth_token = jwt.sign(data, JWT_sig);
-            res.json({ auth_token });
+            success = true;
+            res.json({success, auth_token });
             // res.json(user)
         } catch (err) {
             res.send('Some error occured');
@@ -69,27 +71,29 @@ router.post('/login', [
     body('password', 'invalid password').notEmpty()
 ],
     async (req, res) => {
+        let success = false;
         //Check if user did any mistake in entering fields.
         const result = validationResult(req);
         if (!result.isEmpty()) {
-            return res.status(400).json({ errors: result.array() });
+            return res.status(400).json({ success, errors: result.array() });
         }
         try {
             const { email, password } = req.body;
             let user = await User.findOne({ email });
             if (!user) {
-                return res.status(400).json({ error: 'Pls try to login with correct credentials' });
+                return res.status(400).json({ success, error: 'Pls try to login with correct credentials' });
             }
             const passCompare = bcrypt.compareSync(password, user.password);
             if (!passCompare) {
-                return res.status(400).json({ error: 'Pls try to login with correct credentials' });
+                return res.status(400).json({ success, error: 'Pls try to login with correct credentials' });
             }
             // res.send(user);
             const data = {
                 id: user.id
             };
             const auth_token = jwt.sign(data, JWT_sig);
-            res.json({ auth_token });
+            success = true;
+            res.json({ success, auth_token });
         } catch (err) {
             res.send('Some error occured');
             console.log(err)
